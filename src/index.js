c0a0bee791cae9742ec777f9ab12d308b1f1b8c9
Himank Pathak <Himankpathak@gmail.com>
2020-02-11 14:03:56 +0530

    created redux store in index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import mainReducer from "./reducer";
+import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const initialState = {
+  todos: [{ id: 0, text: "test task", completed: true }],
+  count: [0, 1]
+};
+
+const store = createStore(
+  mainReducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

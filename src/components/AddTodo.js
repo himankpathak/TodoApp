0fd4b0b2bf113542fadd98fe461b331a1f1bbaed
Himank Pathak <himankpathak@gmail.com>
2020-02-12 16:32:25 +0530

    improve validation and minor changes

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,33 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { addTodo } from "../action";
 import { Input } from "semantic-ui-react";
 
-const pushTask = (task, setTask, dispatch) => {
+const pushTask = (task, setTask, setErrorFlag, dispatch) => {
   if (task.trim()) {
     dispatch(addTodo(task));
     setTask("");
   } else {
-    alert("Please enter a valid task");
+    setErrorFlag(true);
   }
 };
 
 const AddTodo = ({ dispatch }) => {
   const [task, setTask] = useState("");
+  const [errorFlag, setErrorFlag] = useState(false);
+  useEffect(() => {
+    if (task.trim()) {
+      setErrorFlag(false);
+    }
+  }, [task]);
+
   return (
     <div style={{ padding: "2vw" }}>
       <Input
         action={{
           content: "Add Todo",
           icon: "add",
-          onClick: () => pushTask(task, setTask, dispatch)
+          onClick: () => pushTask(task, setTask, setErrorFlag, dispatch)
         }}
         value={task}
+        error={errorFlag}
         placeholder="Enter a Todo.."
         onChange={e => setTask(e.target.value)}
         onKeyPress={e => {
           if (e.key === "Enter") {
-            pushTask(task, setTask, dispatch);
+            pushTask(task, setTask, setErrorFlag, dispatch);
           }
         }}
       />
